52c4db215639392634a086a240c9d46af0bafd7d
Renan Tinti <renanoliveira@deloitte.com>
2022-08-30 09:09:30 -0300

    project almost finalized

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from .models import Order
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+# UserCreationForm is a django class for user creation. CreateUserForm is a class that inherit from UserCreationForm, so we can edit it. The CreateUserForm is the one we will be using in the views
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1', 'password2']
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
     path('', views.home, name='home'),
     path('product/', views.product, name='product'),
     path('customer/<str:pk_test>/', views.customer, name='customer'),
